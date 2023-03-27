8140979fe0cac246284fdd329098037d683a6b8f
minhouu <tortor1403@gmail.com>
2023-03-27 16:33:22 +0900

    [feat] create SW Draw Web, version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 2023-1-SNUSW-draw-web
+
 2023-1 SNUSW draw webpage project
---
+++ mh-draw.js
@@ -0,0 +1,71 @@
+const students36 = [
+  "강건희",
+  "구민지",
+  "권준영",
+  "김근태",
+  "김서은",
+  "김어진",
+  "김연우",
+  "김원재",
+  "김재무",
+  "김재형",
+  "김현성",
+  "문소원",
+  "문희태",
+  "박선영",
+  "배지윤",
+  "설예원",
+  "안채연",
+  "유하은",
+  "유혜린",
+  "윤수민",
+  "이경나",
+  "이다예",
+  "이범진",
+  "이승연",
+  "전강훈",
+  "전아영",
+  "정민철",
+  "정승연",
+  "정승우",
+  "정재문",
+  "조세린",
+  "최민호",
+  "최정인",
+  "최주현",
+  "표하영",
+  "홍아윤",
+];
+
+function shuffle(array) {
+  for (let index = array.length - 1; index > 0; index--) {
+    const randomPosition = Math.floor(Math.random() * (index + 1));
+    const temp = array[index];
+    array[index] = array[randomPosition];
+    array[randomPosition] = temp;
+  }
+  return array;
+}
+
+const group = document.querySelectorAll(".dgt-group");
+
+// const studentShuffled = shuffle(students36);
+// [...group].forEach(function (currentValue, currentIndex, listObj) {
+//   const stdNum = currentIndex * 4;
+//   currentValue.innerText = `${currentIndex + 1}조 : ${students36[stdNum]}, ${
+//     students36[stdNum + 1]
+//   }, ${students36[stdNum + 2]}, ${students36[stdNum + 3]}`;
+// });
+
+const runButton = document.querySelector(".dgt-draw-button");
+runButton.addEventListener("click", function printShuffle() {
+  const studentShuffled = shuffle(students36);
+  [...group].forEach(function (currentValue, currentIndex, listObj) {
+    const stdNum = currentIndex * 4;
+    currentValue.innerHTML = `<div class="dgt-group"><b>${
+      currentIndex + 1
+    }조 : (조장) ${students36[stdNum]}</b>, ${students36[stdNum + 1]}, ${
+      students36[stdNum + 2]
+    }, ${students36[stdNum + 3]}</div>`;
+  });
+});
