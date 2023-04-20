87ddbd13eee0d7570ba6b0c6ad9e5fab7323cffc
minhouu <tortor1403@gmail.com>
2023-04-20 14:57:57 +0900

    [update] code for draw updated

--- adstSW-draw.js
+++ adstSW-draw.js
@@ -41,20 +41,20 @@ runButton.addEventListener("click", function printShuffle() {
   shuffle(students22);
   let stdNum = 0;
   [...group].forEach(function (currentValue, currentIndex, listObj) {
-    if (currentIndex === 5 || currentIndex === 4) {
-      currentValue.innerHTML = `<div class="dgt-group"><b>${
-        currentIndex + 1
-      }조 : (조장) ${students22[stdNum]}</b>, ${students22[stdNum + 1]}, ${
-        students22[stdNum + 2]
-      }, ${students22[stdNum + 3]}</div>`;
-      stdNum += 4;
-    } else {
+    if (currentIndex === 0 || currentIndex === 1) {
       currentValue.innerHTML = `<div class="dgt-group"><b>${
         currentIndex + 1
       }조 : (조장) ${students22[stdNum]}</b>, ${students22[stdNum + 1]}, ${
         students22[stdNum + 2]
       }</div>`;
       stdNum += 3;
+    } else {
+      currentValue.innerHTML = `<div class="dgt-group"><b>${
+        currentIndex + 1
+      }조 : (조장) ${students22[stdNum]}</b>, ${students22[stdNum + 1]}, ${
+        students22[stdNum + 2]
+      }, ${students22[stdNum + 3]}</div>`;
+      stdNum += 4;
     }
   });
 });
